b6c051d729c113ccec314c7ffa1ed304d85f8967
anas <muhammedanas5186@gmail.com>
2021-12-03 05:27:57 -0800

    Login Activity created

---
+++ EducationApp/app/src/main/java/com/example/educationapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.educationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var actionBar= supportActionBar
+        actionBar?.hide()
+    }
+}
---
+++ EducationApp/app/src/main/java/com/example/educationapp/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package com.example.educationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        window.statusBarColor = this.resources.getColor(R.color.colorMain)
+
+    }
+}
