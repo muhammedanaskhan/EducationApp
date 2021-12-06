44ab5b0d4c5740f5e2ee9a9d080e57fc130308ab
anas <muhammedanas5186@gmail.com>
2021-12-06 06:04:24 -0800

    334 setting up the base activity

--- EducationApp/app/src/androidTest/java/com/example/educationapp/activities/BaseActivity.kt
+++ EducationApp/app/src/androidTest/java/com/example/educationapp/activities/BaseActivity.kt
@@ -1,11 +1,69 @@
-package com.example.educationapp
+package com.example.educationapp.activities
 
+import android.app.Dialog
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.educationapp.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
-class BaseActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var mProgressDialog: Dialog
+
+    private var doublePressToExitApp = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
     }
+
+    fun showProgressDialog(text: String){
+
+        mProgressDialog  = Dialog(this)
+
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+
+        mProgressDialog.tv_progress_text.text = text
+
+        mProgressDialog.show()
+
+    }
+
+    fun hideProgressDialog(){
+        mProgressDialog.dismiss()
+    }
+
+    fun getCurrentUserID(): String{
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    fun doubleBackToExit(){
+        if(doublePressToExitApp){
+            super.onBackPressed()
+            return
+        }
+        this.doublePressToExitApp = true
+        Toast.makeText(this,"click back once again to exit",Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({doublePressToExitApp = false},2000)
+
+    }
+
+    fun showErrorSnackBar(message: String){
+        val snackBar = Snackbar.make(findViewById(android.R.id.content),message, Snackbar.LENGTH_LONG)
+
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(ContextCompat.getColor(this,R.color.snackbar_error_color))
+
+        snackBar.show()
+    }
+
+
+
+
 }
