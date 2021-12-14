7fc0ffbf0dad56e3412d0996fe5fecdb608dbeb2
anas <muhammedanas5186@gmail.com>
2021-12-14 07:44:46 -0800

    navigation bar and slider panel

--- EducationApp/app/src/main/java/com/example/educationapp/activities/MainActivity.kt
+++ EducationApp/app/src/main/java/com/example/educationapp/activities/MainActivity.kt
@@ -2,14 +2,45 @@ package com.example.educationapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.educationapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var actionBar= supportActionBar
-        actionBar?.hide()
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home_item -> Toast.makeText(applicationContext,"clicked item1 ",Toast.LENGTH_SHORT).show()
+                R.id.favourites_item -> Toast.makeText(applicationContext,"clicked item2 ",Toast.LENGTH_SHORT).show()
+                R.id.logout_item -> Toast.makeText(applicationContext,"clicked item3 ",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
